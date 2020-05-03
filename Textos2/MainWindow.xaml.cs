6b02668e3e3e54037756252d23800aadbf4a1eb8
alexismorison95 <alexismorison83@gmail.com>
2020-05-02 21:27:03 -0300

    primer commit

---
+++ Textos2/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Textos2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void quitarJustificado()
+        {
+            char[] separator = { ' ' };
+
+            string[] palabras = textBoxOriginal.Text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            string oracion = "";
+
+            foreach (string palabra in palabras)
+            {
+                oracion += $"{palabra} ";
+            }
+
+            textBoxFormateado.Text = oracion;
+        }
+
+        private void btnCopiar_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(textBoxFormateado.Text);
+        }
+
+        private void btnLimpiarOriginal_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxOriginal.Text = "";
+        }
+
+        private void btnActualizar_Click(object sender, RoutedEventArgs e)
+        {
+            quitarJustificado();
+        }
+
+        private void textBoxOriginal_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textBoxOriginal.SelectAll();
+        }
+
+        private void textBoxOriginal_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            quitarJustificado();
+        }
+
+        private void textBoxFormateado_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textBoxFormateado.SelectAll();
+        }
+    }
+}
